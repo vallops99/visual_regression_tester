c3d76635c67fc27479ed42317952aaeb970f242e
Valerio Farrotti <valerio.farrotti@gmail.com>
2022-11-15 17:04:09 +0100

    Mapped steps action with functions

--- backend/src/utils/Constants.ts
+++ backend/src/utils/Constants.ts
@@ -1,2 +1,25 @@
-export const LISTENER_WAITER_ACTION = "waitListener";
-export const LISTENERS_ACTIONS = ["waitForNavigation", "waitForNetworkIdle"];
+import { Actions } from "@prisma/client";
+
+import { addListener, login, waitListener, firePageMethod } from "src/Tester";
+
+// This map describes which function an action should call, every single
+// actions that can be stored in the DB (Actions type) must be a key with a value.
+export const ACTIONS_TO_FUNCTIONS: { [key in Actions]: Function } = {
+    login: login,
+    tap: firePageMethod,
+    type: firePageMethod,
+    goto: firePageMethod,
+    click: firePageMethod,
+    focus: firePageMethod,
+    hover: firePageMethod,
+    goBack: firePageMethod,
+    reload: firePageMethod,
+    select: firePageMethod,
+    goForward: firePageMethod,
+    waitListener: waitListener,
+    setGeolocation: firePageMethod,
+    waitForNavigation: addListener,
+    waitForTimeout: firePageMethod,
+    waitForNetworkIdle: addListener,
+    waitForSelector: firePageMethod,
+};
